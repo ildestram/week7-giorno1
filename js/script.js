2c02dd39765a67615697b9401629f252c38ebf41
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2022-10-10 15:09:48 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,30 @@
+class dati {
+    constructor(nome, cognome, age) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.age = age;
+    }
+}
+
+let button = document.querySelector('button');
+
+button.onclick = function() {
+    let nome = document.querySelector('input[name="nome"]').value;
+    let cognome = document.querySelector('input[name="cognome"]').value;
+    let age = document.querySelector('input[name="age"]').value;
+    let datiAnagrafici = new Array(nome, cognome, age);
+
+    console.log(datiAnagrafici);
+    nome.value = '';
+    cognome.value = '';
+    age.value = '';
+
+    aggiungi();
+}
+
+function aggiungi (event) {
+    let firstRow = document.querySelector('.one');
+    let printOne = document.querySelector('td');
+    printOne.innerHTML = datiAnagrafici.length[0];
+    firstRow.appendChild(printOne);
+}
